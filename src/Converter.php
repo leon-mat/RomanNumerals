b23af9b78c17005faf49d0be5b8eb5c780fb640c
leon-mat <l.leonmatthieu@gmail.com>
2016-10-13 18:16:33 +0200

    poulpe

--- src/Converter.php
+++ src/Converter.php
@@ -8,18 +8,58 @@ namespace RomanNumerical;
 
 class Converter
 {
+    private $numberToRoman = [
+        'I' => 1,
+        'IV' => 4,
+        'V' => 5,
+        'IX' => 9,
+        'X' => 10,
+        'XL' => 40,
+        'L' => 50,
+        'XC' => 90,
+        'C' => 100,
+        'CD' => 400,
+        'D' => 500,
+        'CM' => 900,
+        'M' => 1000
+    ];
 
     /**
      * Converter constructor.
      */
     public function __construct()
     {
+        $this->numberToRoman = array_reverse($this->numberToRoman);
     }
 
-    public function convert($number) {
-        if (10 === $number) {
-            return 'X';
+    /**
+     * @param int $number
+     * @param int $index
+     * @param string $resultFinal
+     * @return string
+     */
+    private function recursive($number, $index, $resultFinal)
+    {
+        if ($number == 0) {
+            return $resultFinal;
         }
-        return 'V';
+
+        $letter = array_keys($this->numberToRoman)[$index];
+        $value = array_values($this->numberToRoman)[$index];
+
+        $testLetter = $number / $value;
+        if ($testLetter < 1) {
+            return $this->recursive($number, $index+1, $resultFinal);
+        } else {
+            return $this->recursive($number - $value, 0, $resultFinal . $letter);
+        }
+    }
+
+    /**
+     * @param int $number
+     * @return string
+     */
+    public function convert($number) {
+        return $this->recursive($number, 0, '');
     }
 }
--- tests/RomanNumeralTest.php
+++ tests/RomanNumeralTest.php
@@ -22,9 +22,38 @@ class RomanNumeralTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($converter->convert(5), 'V');
     }
 
-    public function test_CC()
+    public function test_C()
     {
         $converter = new Converter();
         $this->assertEquals($converter->convert(200), 'CC');
+    }
+
+    public function test_Rand_X()
+    {
+        $converter = new Converter();
+
+        $res = $converter->convert(rand(10, 19));
+        $hasLetter = in_array('X', str_split($res));
+        $this->assertEquals($hasLetter, true);
+    }
+
+    public function test_Rand_V_Per_Dix()
+    {
+        $converter = new Converter();
+
+        foreach (range(0, 9) as $i) {
+            $res = $converter->convert($i * 10 + 4);
+            $this->stringContains('IV', $res);
+        }
+    }
+
+    public function test_yolo()
+    {
+        $converter = new Converter();
+        $this->assertEquals($converter->convert(400), 'CD');
+        $this->assertEquals($converter->convert(404), 'CDIV');
+        $this->assertEquals($converter->convert(349), 'CCCXLIX');
+        $this->assertEquals($converter->convert(494), 'CDXCIV');
+        $this->assertEquals($converter->convert(48), 'XLVIII');
     }
 }
